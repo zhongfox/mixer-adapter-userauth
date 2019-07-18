2ddb7f5dedb8e29bad15661fc192c4fc188db13d
foxzhong <foxzhong@tencent.com>
2019-07-18 10:56:34 +0800

    useruauth go

---
+++ userauth/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	userauth "istio.io/istio/mixer/adapter/userauth"
+)
+
+func main() {
+	addr := ""
+	if len(os.Args) > 1 {
+		addr = os.Args[1]
+	}
+
+	s, err := userauth.NewAuthAdapter(addr)
+	if err != nil {
+		fmt.Printf("unable to start server: %v", err)
+		os.Exit(-1)
+	}
+
+	shutdown := make(chan error, 1)
+	go func() {
+		s.Run(shutdown)
+	}()
+	_ = <-shutdown
+}
